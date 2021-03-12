eca2f4529c47647f9a08f3dab2119df8a83d436d
Romain Pereira <pereira.romain01@gmail.com>
2021-03-12 15:36:27 +0100

    oui

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, SafeAreaView, StatusBar, FlatList, View, TouchableOpacity } from 'react-native';
+import TodoInput from "./components/TodoInput";
+import TodoItem from "./components/TodoItem";
 
-export default function App() {
+const App = () => {
+  const [todoItems, setTodoItems] = useState([{ text: "Buy groceries", completed: true }, { text: "Make blogpost", completed: false }]);
+
+  // Add a new item to the state
+  function addTodoItem(_text) {
+    setTodoItems([...todoItems, { text: _text, completed: false }]);
+  }
+
+  // Delete an item from state by index
+  function deleteTodoItem(_index) {
+    let tempArr = [...todoItems];
+    tempArr.splice(_index, 1);
+    setTodoItems(tempArr)
+  }
+
+  // Function to set completed to true by index.
+  function completeTodoItem(_index) {
+    let tempArr = [...todoItems];
+    tempArr[_index].completed = true;
+    setTodoItems(tempArr)
+  }
+
+  // Render
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle={"light-content"} backgroundColor={"#212121"} />
+      <SafeAreaView style={{ margin: 10, justifyContent: 'center', flex: 1 }}>
+        <Text style={{ fontSize: 36, fontWeight: 'bold', marginLeft: '40%' }}>Todo</Text>
+        <FlatList
+          data={todoItems}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item, index }) => {
+            return (
+              <TodoItem
+                item={item}
+                deleteFunction={() => deleteTodoItem(index)}
+                completeFunction={() => completeTodoItem(index)}
+              />
+            )
+          }}
+        />
+        <TodoInput onPress={addTodoItem} />
+      </SafeAreaView>
+    </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
